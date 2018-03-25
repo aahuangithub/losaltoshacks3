83bf266bd211e514eddfc99e3668c7291b659dbd
aaron <aahuang@s.sfusd.edu>
2018-03-24 19:39:40 -0700

    finish maps

--- FindersSweepers/app/src/main/java/tech/trash/finderssweepers/Login.java
+++ FindersSweepers/app/src/main/java/tech/trash/finderssweepers/Login.java
@@ -1,5 +1,6 @@
 package tech.trash.finderssweepers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,10 +12,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        finders = findViewById(R.id.finders_button);
         finders.setOnClickListener((v)->{
             Log.d("Login Button Clicked", "Finders pressed");
         });
+        sweepers = findViewById(R.id.sweepers_button);
         sweepers.setOnClickListener((view -> {
+            startActivity(new Intent(getApplicationContext(), MapsActivity.class));
             Log.d("Login Button Clicked", "Sweepers pressed");
         }));
     }
--- FindersSweepers/app/src/main/java/tech/trash/finderssweepers/MapsActivity.java
+++ FindersSweepers/app/src/main/java/tech/trash/finderssweepers/MapsActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -17,11 +17,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setContentView(R.layout.activity_collector);
+        MapFragment mp = MapFragment.newInstance();
+        mp.getMapAsync(this);
+        getFragmentManager().beginTransaction().add(R.id.map_frame, mp).commit();
+
+
     }
 
 
@@ -39,7 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(-150, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
